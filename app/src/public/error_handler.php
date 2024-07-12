fbc7a9b256c21ba020a026aac93d90e3b17446d2
Anderson Alcará Alvarenga <anderalvarenga@gmail.com>
2024-07-12 14:56:56 -0300

    feature: not found error handling

---
+++ app/src/public/error_handler.php
@@ -0,0 +1,7 @@
+<?php
+
+http_response_code(404);
+
+require_once '404.html';
+
+exit;
--- app/src/public/index.php
+++ app/src/public/index.php
@@ -4,15 +4,9 @@ require_once '../controllers/Controller.php';
 require_once '../models/Model.php';
 
 $requestUri = $_SERVER['REQUEST_URI'];
-echo "<br> requestUri: ", $requestUri;
 $requestUri = filter_var($requestUri, FILTER_SANITIZE_URL);
-echo "<br> requestUri: ", $requestUri;
 $requestUri = $requestUri == "/" ? "home" : trim($requestUri, '/');
-echo "<br> requestUri: ", $requestUri;
 $url = explode('/', $requestUri);
-for ($i = 0; $i < count($url); $i++) {
-	echo "<br>url[$i]: ", $url[$i];
-}
 
 $controllerName = ucfirst(array_shift($url)) . 'Controller';
 $methodName = array_shift($url) ?? 'index';
@@ -24,8 +18,8 @@ if (file_exists("../controllers/$controllerName.php")) {
 	if (method_exists($controller, $methodName)) {
 		call_user_func_array([$controller, $methodName], $params);
 	} else {
-		echo "Method not found!";
+		require_once 'error_handler.php';
 	}
 } else {
-	echo "Controller not found!";
+	require_once 'error_handler.php';
 }
