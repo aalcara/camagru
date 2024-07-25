22e7ecfb7546f9072406ae0f71403b2460909d95
Anderson Alcará Alvarenga <anderalvarenga@gmail.com>
2024-07-25 12:59:58 -0300

    feature: server overlays the images

--- app/src/controllers/DashboardController.php
+++ app/src/controllers/DashboardController.php
@@ -12,14 +12,14 @@ class DashboardController extends Controller
 		$this->canvas_width = 320;
 		$this->canvas_height = 240;
 		$this->superposable_images = [
-			"Mr. Bean" => "images/mr-bean.png",
-			"kitty" => "images/kitty.png",
+			"Mr. Bean" => "/images/mr-bean.png",
+			"kitty" => "/images/kitty.png",
 		];
 	}
 
 	public function index()
 	{
-		$this->view("dashboard/index", ["canvasWidth" => $this->canvas_width, "canvasHeight" => $this->canvas_height, "superposableImages"=> $this->superposable_images]);
+		$this->view("dashboard/index", ["canvasWidth" => $this->canvas_width, "canvasHeight" => $this->canvas_height, "superposableImages" => $this->superposable_images]);
 	}
 
 	public function upload($params = [])
@@ -52,26 +52,49 @@ class DashboardController extends Controller
 		}
 
 		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-			if (!isset($_POST['image'])) {
+			if (!isset($_POST['image']) || !isset($_POST['superposableImage'])) {
 				echo "No image data recieved";
 				exit;
 			}
 			$image_data = $_POST['image'];
+			$superposable_image_path = $_POST['superposableImage'];
 
 			[, $image_data] = explode(';', $image_data);
 			[, $image_data] = explode(',', $image_data);
 			$decoded_image = base64_decode($image_data);
 
-			$file_hash = hash('sha256', $decoded_image);
-			$file_path = "../../uploads/{$file_hash}.png";
+			$image = imagecreatefromstring($decoded_image);
+			if ($image === false) {
+				echo "Failed to create image from base64 data";
+				exit;
+			}
 
-			if (!file_put_contents($file_path, $decoded_image)) {
+			$superposable_image = imagecreatefrompng(".{$superposable_image_path}");
+			if ($superposable_image === false) {
+				echo "Failed to load superposable image";
+				exit;
+			}
+
+			$image_width = imagesx($image);
+			$image_height = imagesy($image);
+
+			imagealphablending($image, true);
+			imagesavealpha($image, true);
+			imagecopy($image, $superposable_image, 0, 0, 0, 0, $image_width, $image_height);
+
+			ob_start();
+			imagepng($image);
+			$final_image_data = ob_get_contents();
+			ob_end_clean();
+
+			$file_hash = hash('sha256', $final_image_data);
+			$file_path = "../../uploads/{$file_hash}.png";
+			if (!file_put_contents($file_path, $final_image_data)) {
 				echo "Fail uploading image";
 			}
 
 			$image_model = $this->model("Image");
 			$image_id = $image_model->createImage($_SESSION['user_id'], $file_hash);
-
 			if (!$image_id) {
 				echo "Fail saving image to databse";
 				exit;
--- db/init.sql
+++ db/init.sql
@@ -17,3 +17,19 @@ CREATE TABLE IF NOT EXISTS images (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (owner_id) REFERENCES users (id)
 );
+
+INSERT INTO
+    users (id, username, email, password)
+VALUES (
+        '1',
+        'asd',
+        'testuser@example.com',
+        '$2y$10$Ac36JNb28dNxfUz2j4jI6OHXSlF8/0TtvatWFoTISMEyqVkYqraRK'
+    ),
+    (
+        '2',
+        'john_doe',
+        'john.doe@example.com',
+        'password2'
+    );
+
