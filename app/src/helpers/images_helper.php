a3770b6f32d9fb16af846b74e926b8c59c0b87c6
Anderson Alcará Alvarenga <anderalvarenga@gmail.com>
2024-07-26 07:49:19 -0300

    fix: upload path

--- app/src/helpers/images_helper.php
+++ app/src/helpers/images_helper.php
@@ -4,11 +4,11 @@ function image_hash_to_path($hash)
 {
 	$image_hash_hex = bin2hex($hash);
 
-	$file_path = "./uploads/{$image_hash_hex}.png";
+	$file_path = "/var/www/html/public/uploads/{$image_hash_hex}.png";
 	if (!file_exists($file_path)) {
 		echo "File does not exist";
 		return;
 	}
 
-	return substr($file_path, 1);
+	return str_replace("/var/www/html/public", "", $file_path);
 }
